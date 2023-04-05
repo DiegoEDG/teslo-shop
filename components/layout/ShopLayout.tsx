636bd263417e306976e92bf2c4a6b9b997f7fc1c
Digudev <1.5.dg.diego@gmail.com>
2023-04-05 10:43:01 -0600

    UI: Components Created

---
+++ components/layout/ShopLayout.tsx
@@ -0,0 +1,42 @@
+import { FC, PropsWithChildren } from 'react';
+import Head from 'next/head';
+import { Navbar, SideMenu } from '../ui';
+
+interface Props {
+	title: string;
+	pageDescription: string;
+	imageFullUrl?: string;
+}
+
+export const ShopLayout: FC<PropsWithChildren<Props>> = ({ children, title, pageDescription, imageFullUrl }) => {
+	return (
+		<>
+			<Head>
+				<title>{title}</title>
+
+				<meta name="description" content={pageDescription} />
+
+				<meta name="og:title" content={title} />
+				<meta name="og:description" content={pageDescription} />
+
+				{imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+			</Head>
+			<nav>
+				<Navbar />
+			</nav>
+			<main
+				style={{
+					margin: '60px auto',
+					maxWidth: '1440px',
+					padding: '0px 15px',
+					display: 'block'
+				}}
+			>
+				{children}
+			</main>
+			<SideMenu />
+			{/* Footer */}
+			<footer>{/* TODO: mi custom footer */}</footer>
+		</>
+	);
+};
---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,50 @@
+import NextLink from 'next/link';
+
+import { AppBar, Badge, Box, Button, IconButton, Link, Toolbar, Typography } from '@mui/material';
+import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
+
+export const Navbar = () => {
+	return (
+		<AppBar>
+			<Toolbar>
+				<NextLink href="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
+					<Typography variant="h6" fontWeight="600">
+						TesloShop
+					</Typography>
+				</NextLink>
+
+				<Box flex={1} />
+
+				<Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+					<NextLink href="/xd" style={{ textDecoration: 'none', marginRight: '10px' }}>
+						<Button variant="outlined">Hombres</Button>
+					</NextLink>
+					<NextLink href="/" style={{ textDecoration: 'none', marginRight: '10px' }}>
+						<Button variant="outlined">Mujeres</Button>
+					</NextLink>
+					<NextLink href="/" style={{ textDecoration: 'none' }}>
+						<Button variant="outlined">Niños</Button>
+					</NextLink>
+				</Box>
+
+				<Box flex={1} />
+
+				<IconButton>
+					<SearchOutlined />
+				</IconButton>
+
+				<NextLink href="/">
+					<IconButton>
+						<Badge badgeContent={2} color="secondary">
+							<ShoppingCartOutlined />
+						</Badge>
+					</IconButton>
+				</NextLink>
+
+				<Button variant="outlined" sx={{ marginLeft: '10px' }}>
+					Menú
+				</Button>
+			</Toolbar>
+		</AppBar>
+	);
+};

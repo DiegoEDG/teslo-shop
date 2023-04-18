6ab20d6dcf967539f66cd1910fdca7f5c977b6ea
Digudev <1.5.dg.diego@gmail.com>
2023-04-18 09:58:44 -0600

    FEAT [AUTH]: Handle login and signup forms

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,36 +1,95 @@
+import { useState } from 'react';
 import NextLink from 'next/link';
-import { Box, Button, Grid, TextField, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { Box, Button, Chip, Grid, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../../../components/layout';
+import { isEmail } from '../../../utils';
+import { tesloApi } from '../../../api';
+import { ErrorOutline } from '@mui/icons-material';
+
+type FormData = {
+	email: string;
+	password: string;
+};
 
 const LoginPage = () => {
+	const [showError, setShowError] = useState(false);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<FormData>();
+
+	const onLogin = async (formData: FormData) => {
+		try {
+			setShowError(false);
+			await tesloApi.post('/user/login', formData);
+		} catch (error) {
+			setShowError(true);
+			setTimeout(() => {
+				setShowError(false);
+			}, 3000);
+		}
+	};
+
 	return (
 		<AuthLayout title={'Ingresar'}>
-			<Box sx={{ width: 600, padding: '10px 20px' }}>
-				<Grid container spacing={2}>
-					<Grid item xs={12}>
-						<Typography variant="h1" component="h1">
-							Login
-						</Typography>
-					</Grid>
+			<form onSubmit={handleSubmit(onLogin)}>
+				<Box sx={{ width: 600, padding: '10px 20px' }}>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<Typography variant="h1" component="h1">
+								Login
+							</Typography>
+							<Chip
+								label="Email or password not valid"
+								color="error"
+								icon={<ErrorOutline />}
+								className="fadeIn"
+								sx={{ display: showError ? 'flex' : 'none', marginTop: '10px', paddingY: '25px' }}
+							/>
+						</Grid>
 
-					<Grid item xs={12}>
-						<TextField label="Email" variant="filled" fullWidth />
-					</Grid>
-					<Grid item xs={12}>
-						<TextField label="Password" type="password" variant="filled" fullWidth />
-					</Grid>
+						<Grid item xs={12}>
+							<TextField
+								label="Email"
+								variant="filled"
+								fullWidth
+								{...register('email', {
+									required: 'Email is required',
+									validate: isEmail
+								})}
+								error={!!errors.email}
+								helperText={errors.email?.message}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								label="Password"
+								type="password"
+								variant="filled"
+								fullWidth
+								{...register('password', {
+									required: 'Password is required',
+									minLength: { value: 6, message: 'Password must be at least 6 characters' }
+								})}
+								error={!!errors.password}
+								helperText={errors.password?.message}
+							/>
+						</Grid>
 
-					<Grid item xs={12}>
-						<Button color="primary" className="circular-btn" size="large" fullWidth>
-							Log In
-						</Button>
-					</Grid>
+						<Grid item xs={12}>
+							<Button color="primary" className="circular-btn" size="large" fullWidth type="submit">
+								Log In
+							</Button>
+						</Grid>
 
-					<Grid item xs={12} display="flex" justifyContent="end">
-						<NextLink href="/auth/register">Does´t have an account?</NextLink>
+						<Grid item xs={12} display="flex" justifyContent="end">
+							<NextLink href="/auth/register">Does´t have an account?</NextLink>
+						</Grid>
 					</Grid>
-				</Grid>
-			</Box>
+				</Box>
+			</form>
 		</AuthLayout>
 	);
 };
--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,39 +1,110 @@
+import { useState } from 'react';
 import NextLink from 'next/link';
-import { Box, Button, Grid, TextField, Typography } from '@mui/material';
+import { Box, Button, Chip, Grid, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../../../components/layout';
+import { useForm } from 'react-hook-form';
+import { tesloApi } from '../../../api';
+import { ErrorOutline } from '@mui/icons-material';
+import { isEmail } from '../../../utils';
+
+type FormData = {
+	name: string;
+	email: string;
+	password: string;
+};
 
 const RegisterPage = () => {
+	const [showError, setShowError] = useState(false);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<FormData>();
+
+	const onSignUp = async (formData: FormData) => {
+		try {
+			setShowError(false);
+			const { data } = await tesloApi.post('/user/register', formData);
+			console.log(data);
+		} catch (error) {
+			setShowError(true);
+			setTimeout(() => {
+				setShowError(false);
+			}, 3000);
+		}
+	};
+
 	return (
 		<AuthLayout title={'Ingresar'}>
-			<Box sx={{ width: 600, padding: '10px 20px' }}>
-				<Grid container spacing={2}>
-					<Grid item xs={12}>
-						<Typography variant="h1" component="h1">
-							Create an account
-						</Typography>
-					</Grid>
+			<form onSubmit={handleSubmit(onSignUp)}>
+				<Box sx={{ width: 600, padding: '10px 20px' }}>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<Typography variant="h1" component="h1">
+								Create an account
+							</Typography>
+							<Chip
+								label="Email is already registered"
+								color="error"
+								icon={<ErrorOutline />}
+								className="fadeIn"
+								sx={{ display: showError ? 'flex' : 'none', marginTop: '10px', paddingY: '25px' }}
+							/>
+						</Grid>
 
-					<Grid item xs={12}>
-						<TextField label="Name" variant="filled" fullWidth />
-					</Grid>
-					<Grid item xs={12}>
-						<TextField label="Email" variant="filled" fullWidth />
-					</Grid>
-					<Grid item xs={12}>
-						<TextField label="Password" type="password" variant="filled" fullWidth />
-					</Grid>
+						<Grid item xs={12}>
+							<TextField
+								label="Name"
+								variant="filled"
+								fullWidth
+								{...register('name', {
+									required: 'Name is required',
+									minLength: { value: 4, message: 'Name must be at least 4 characters' }
+								})}
+								error={!!errors.name}
+								helperText={errors.name?.message}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								label="Email"
+								variant="filled"
+								fullWidth
+								{...register('email', {
+									required: 'Email is required',
+									validate: isEmail
+								})}
+								error={!!errors.email}
+								helperText={errors.email?.message}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								label="Password"
+								type="password"
+								variant="filled"
+								fullWidth
+								{...register('password', {
+									required: 'Password is required',
+									minLength: { value: 6, message: 'Password must be at least 6 characters' }
+								})}
+								error={!!errors.password}
+								helperText={errors.password?.message}
+							/>
+						</Grid>
 
-					<Grid item xs={12}>
-						<Button color="secondary" className="circular-btn" size="large" fullWidth>
-							Sign Up
-						</Button>
-					</Grid>
+						<Grid item xs={12}>
+							<Button color="secondary" className="circular-btn" size="large" fullWidth type="submit">
+								Sign Up
+							</Button>
+						</Grid>
 
-					<Grid item xs={12} display="flex" justifyContent="end">
-						<NextLink href="/auth/login">Already have an account?</NextLink>
+						<Grid item xs={12} display="flex" justifyContent="end">
+							<NextLink href="/auth/login">Already have an account?</NextLink>
+						</Grid>
 					</Grid>
-				</Grid>
-			</Box>
+				</Box>
+			</form>
 		</AuthLayout>
 	);
 };
